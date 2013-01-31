b7942c0db9101d1c01a69bb7a490dca87c24bcca
Troy Stevenson <troy3580@fullsail.edu>
2013-01-30 22:51:51 -0800

    re watched week 2 and 3 to find errors

--- js/main.js
+++ js/main.js
@@ -13,26 +13,26 @@ window.addEventListener("DOMContentLoaded", function () {
 	}
 
 //Creat select field element and populate with options.
-	function jobs () {
+	function makeJobs () {
 		var formTag = document.getElementsByTagName("form");  //formTag is an Array of all the tag names
-		var selectLi = $("select");
-		var makeSelect = document.createElement("select");
+ 		    selectLi = $("select"),
+ 		    makeSelect = document.createElement("select");
 		    makeSelect.setAttribute("id", "groups");
-		for(var i = 0, j=jobGroups.length; i<j; i++){
+		for(var i=0, j=jobGroups.length; i<j; i++){
 			var makeOption = document.createElement("option");
 			var optText = jobGroups[i];
 			makeOption.setAttribute("value", optText);
 			makeOption.innerHTML = optText;
 			makeSelect.appendChild(makeOption);
 		
-		selectLi.appendChild(makeSelect);
 		}
+		selectLi.appendChild(makeSelect);
 	}		
 
-	//value of fange slider
+//value of fange slider
 	function sliderValue (slideAmount){
 		var display = document.getElementById("reliable");
-		display.innerHTML = slideAmount
+		display.innerHTML = slideAmount;
     }
 
 
@@ -41,10 +41,10 @@ window.addEventListener("DOMContentLoaded", function () {
 
 //Find the value of selected radio button.
 	function getSelectedRadio () {
-	 	var radios = document.forms[0].shiftTime;
-	 	for(var i = 0; i<radios.length; i++){
-		 	if(radios[i].checked){
-		 		shiftValue = radios[i].value;
+	 	var radio = document.forms[0].time;
+	 	for(var i=0; i<radio.length; i++){
+		 	if(radio[i].checked){
+		 		timeValue = radio[i].value;
 		 	}
 	 	}
  	}
@@ -55,13 +55,13 @@ window.addEventListener("DOMContentLoaded", function () {
 		 	case "on":
 		 		$("contactForm").style.display = "none";
 		 		$("clear").style.display       = "inline";
-		 		$("showLink").style.display    = "none";
+		 		$("displayLink").style.display = "none";
 		 		$("addNew").style.display      = "inline";
 		 		break;
 		 	case "off":
 		 		$("contactForm").style.display = "block";
 		 		$("clear").style.display       = "inline";
-		 		$("showLink").style.display    = "block";
+		 		$("displayLink").style.display = "inline";
 		 		$("addNew").style.display      = "none";
 		 		$("items").style.display       = "none";
 		 		break;
@@ -74,38 +74,32 @@ window.addEventListener("DOMContentLoaded", function () {
 		if (!key){
 	 		var id = Math.floor(Math.random ()*10000000001);
 	 	}else{
-		 	//set the id to the existing key we're editing tso that it will save over data
-		 	//the key is the same key thats been passed along from the edit Submit handler
-		 	//to validate function, then passed there, in to the storeData Function
+//set the id to the existing key we're editing tso that it will save over data
+//the key is the same key thats been passed along from the edit Submit handler
+//to validate function, then passed there, in to the storeData Function
 		 	id  = key;
 		}
-	}
 //Gather all Form field value and store in an object
 //Objext properties contain array with the form and input value.
 	 	getSelectedRadio();
 	 	var item                            = {};
-	 		item.group                      = ["Groups:",       $("group").value];
+	 		item.group                      = ["Group:",       $("groups").value];
 	 		item.fName                      = ["Firest Name:",  $("fName").value];
 	 		item.lName                      = ["Last Name:",    $("lName").value];
-	 		item.eMail                      = ["Email:",        $("eMail").value];
+	 		item.email                      = ["Email:",        $("email").value];
 	 		item.pNumber                    = ["Phone Number:", $("pNumber").value];
 	 		item.reliable                   = ["Reliable:",     $("reliable").value];
-	 		item.shiftTime                  = ["shift:",shiftTime];
+	 		item.time					    = ["Time:", timeValue];
 	 		item.notes                      = ["Notes:", $("notes").value];
 //Save data into Local storage: Use Stringify to convert our object to a string.
 	 	localStorage.setItem(id, JSON.stringify(item));
 	 	alert("Employee is Saved!"); 	
-// 	}
-    
+ 	}
      	
  	function addNew () {
 	 	window.location.reload();
  	}
-    
-    item.shiftTime                = ["shiftTime", shift];
-    
-    
-    
+        
     function getData () {
 	 	toggleControls("on");
 	 	if(localStorage.length === 0){
@@ -120,9 +114,9 @@ window.addEventListener("DOMContentLoaded", function () {
 	 	makeDiv.appendChild(makeList);
 	 	document.body.appendChild(makeDiv);
 	 	$("items").style.display = "block";
-	 	for(var i = 0, j=localStorage.length; i<j; i++){
+	 	for(var i=0, j=localStorage.length; i<j; i++){
 		 	var makeli = document.createElement("li");
-		 	var linksLi = document.createElement("li");
+   		 	var linksLi = document.createElement("li");
 		 	makeList.appendChild(makeli);
 		 	var key = localStorage.key(i);
 		 	var value = localStorage.getItem(key);
@@ -130,7 +124,7 @@ window.addEventListener("DOMContentLoaded", function () {
 		 	var obj = JSON.parse(value);
 		 	var makeSubList = document.createElement("ul");
 		 	makeli.appendChild(makeSubList);
-		 	getImage(obj.groups[1], makeSubList);
+////////////////////////////////////////////////////////////////////////// 		 	getImage(obj.groups[1], makeSubList);
 		 	for(var n in obj){
 			 	var makeSubli = document.createElement("li");
 			 	makeSubList.appendChild(makeSubli);
@@ -140,10 +134,10 @@ window.addEventListener("DOMContentLoaded", function () {
 		 	}
 		 	makeItemLinks(localStorage.key(i), linksLi); //Create our edit and delete buttons/Link for each item in local storage.
 	 	}
-	}
+	
  	
- 	//get the image for the right job
- 	function getImage(jobName, makeSubList ){
+//get the image for the right job
+ 	function getImage(jobName, makeSubList){
 	 	var imageLi = document.createElement("li");
 	 	makeSubList.appendChild(imageLi);
 	 	var newImg = document.createElement("img");
@@ -152,7 +146,7 @@ window.addEventListener("DOMContentLoaded", function () {
  	}
 
  	function autoFillData(){
-	 	//Storing JSON Object to local storage
+//Storing JSON Object to local storage
 	 	for(var n in json){
 	 		var id = Math.floor(Math.random ()*10000000001);
 	 		localStorage.setItem(id, JSON.stringify(json[n])); 
@@ -164,67 +158,66 @@ window.addEventListener("DOMContentLoaded", function () {
  	function makeItemLinks (key, linksLi){
 	 	
 //add edit single item link
-	 	var editLink                     = document.createElement("a");
-	 	editLink.href                    = "#";
-	 	editLink.key                     = key;
-	 	var editText                     = "Edit Employee";
+	 	var editLink = document.createElement("a");
+	 	editLink.href = "#";
+	 	editLink.key = key;
+	 	var editText = "Edit Employee";
 	 	editLink.addEventListener("click", editItem);
-	 	editLink.innerHTML               = editText;
+	 	editLink.innerHTML = editText;
 	 	linksLi.appendChild(editLink);
 	 	
 //add break
-	 	var breakTag                     = document.createElement("br");
+	 	var breakTag = document.createElement("br");
 	 	linksLi.appendChild(breakTag);
 	 	
 //add delete single item link
-	 	var deleteLink                   = document.createElement("a");
-	 	deleteLink.href                  = "#";
-	 	deleteLink.key                   = key;
-	 	var deleteText                   = "Delete Contact";	
-	 	//deleteLink.addEventListener("click", deleteItem);
-	 	deleteLink.innerHTML             = deleteText;
+	 	var deleteLink = document.createElement("a");
+	 	deleteLink.href = "#";
+	 	deleteLink.key = key;
+	 	var deleteText = "Delete Employee";	
+        deleteLink.addEventListener("click", deleteItem);
+	 	deleteLink.innerHTML = deleteText;
 	 	linksLi.appendChild(deleteLink);
 	}
 	
 	function editItem(){
-		//Grab the data from our item from local storage.
-		var value                         = localStorage.getItem(this.key);
-		var item                          = JSON.parse(value);
+//Grab the data from our item from local storage.
+		var value = localStorage.getItem(this.key);
+		var item = JSON.parse(value);
 		
-		//Show form
+//Show form
 		toggleControls ("off");
 		
-		//populat the form foelds this the current localstorage values
-		$("groups").value                 = item.groups[1];
+//populat the form foelds this the current localstorage values
+		$("groups").value                 = item.group[1];
 		$("fName").value                  = item.fName[1];
 		$("lName").value                  = item.lName[1];
-		$("eMail").value                  = item.eMail[1];
+		$("email").value                  = item.email[1];
 		$("pNumber").value                = item.pNumber[1];
-		var radios                        = document.forms[0].shift;
-		for(var i = 0; i<radios.length; i++){
-			if(radios[i].value = "dayShift" && item.shift[1] == "dayShift"){
+		var radios                        = document.forms[0].time;
+		for(var i=0; i<radios.length; i++){
+			if(radios[i].value = "Day" && item.time[1] == "Day"){
 				radios[i].setAttribute("checked", "checked");
-			}else if(radios[i].value = "nightShift" && item.shift[1] =="nightShift"){
+			}else if(radios[i].value = "Night" && item.time[1] == "Night"){
 				radios[i].setAttribute("checked", "checked");
 			}
 		}
-	
-		$("reliable").value  = item.reliable[1];
-		$("notes").value = item.notes[1];
-		
-		//REmove the initial listener from the input "save contact' button
+		$("reliable").value               = item.reliable[1];
+		$("notes").value                  = item.notes[1];
+	}	
+//REmove the initial listener from the input "save contact' button
 		save.removeEventListener("click", storeData);
-		//Change Submit button value to Edit Button
+//Change Submit button value to Edit Button
 		$("submit").value = "Edit Employee";
 		var editSubmit = $("submit");
-		//save the key value established in this function as a property of the event
-		//so I can use that value when I save
+//save the key value established in this function as a property of the event
+//so I can use that value when I save
 		editSubmit.addEventListener("click", validate);
 		editSubmit.key = this.key;		
-	}
-
+			
+	
 	function deleteItem(){
-		var ask = confirm("Are you sure you want to delete this contact");
+		var ask = confirm("Are you sure you want to delete this contact?");
 		if(ask){
 			localStorage.removeItem(this.key);
 			alert("Employee was deleted!");
@@ -246,88 +239,79 @@ window.addEventListener("DOMContentLoaded", function () {
  	}
 
  	function validate (e){
-	 	//define the elements I want to check
-	 	var getgroups                     = $("groups");
-	 	var getfName                     = $("fName");
-	 	var getlName                     = $("lName");
-	 	var getpNumber                   = $("pNumber");
-	 	var geteMail                     = $("eMail");
+//define the elements I want to check
+	 	var getGroup = $("groups");
+	 	var getFname = $("fName");
+	 	var getLname = $("lName");
+	 	var getEmail = $("email");
 	 	
-	 	//reset the error messages
-	 	errMsg.innerHTML                 = "";
-		getgroups.style.border           = "1px solid red";
-		getfName.style.border            = "1px solid red";
-	 	getlName.style.border            = "1px solid red";
-		getpNumber.style.border          = "1px solid red";
-	 	geteMail.style.border            = "1px solid red";
+//reset the error messages
+	 	errMsg.innerHTML = "";
+		getGroup.style.border = "1px solid black";
+		getFname.style.border = "1px solid black";
+	 	getLname.style.border = "1px solid black";
+	 	getEmail.style.border = "1px solid black";
 
-	 	//Get error messages
-	 	var messageAry                   = [];
-	 	//group validation
-	 	if(getgroups.value               = "--Choose A Group--"){
-		 	var groupError               = "Please choose a job type.";
-		 	getgroups.style.border        = "1px solid red";
+//Get error messages
+	 	var messageAry = [];
+//group validation
+	 	if(getGroup.value === "--Choose A Group--"){
+		 	var groupError = "Please choose a job.";
+		 	getGroup.style.border = "1px solid red";
 		 	messageAry.push(groupError);
 	 	}
 	 }
-	 	//first name valitation
-	 	if(getfName.value                = ""){
-		 	var fNameError               = "Please enter a first name.";
-		 	getfName.style.border        = "1px solid red";
+//first name valitation
+	 	if(getFname.value === ""){
+		 	var fNameError = "Please enter a first name.";
+		 	getFname.style.border        = "1px solid red";
 		 	messageAry.push(fNameError);
-	 	}
+	}
 	 	
-	 	//last name validation
-	 	if(getlName.value                = ""){
-		 	var lNameError               = "Please enter a last name.";
-		 	getlName.style.border        = "1px solid red";
+//last name validation
+	 	if(getLname.value === ""){
+		 	var lNameError = "Please enter a last name.";
+		 	getLname.style.border = "1px solid red";
 		 	messageAry.push(LnameError);
-	 	}
+	}
 	 	
-	 	//Phone number Valitation
-//	 	var numberVal                  = /^(?!.*-.*-.*-)(?=(?:\d{8,10}$)|(?:(?=.{9,11}$)[^-]*-[^-]*$)|(?:(?=.{10,12}$)[^-]*-[^-]*-[^-]*$)  )[\d-]+$/;
-//	 	if(!(numberVal.exec(getPnumber.value))){
-//		 	var phoneError                = "Please enter a valid phone number.";
-//		 	getPnumber.style.border       = "1px solid red";
-//		 	messageAry.push(phoneError);
-//	 	}
-	 
-	 	//email valitation
+//email valitation
 	 	var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-	 	if(!(re.exec(geteMail.value))){
+	 	if(!(re.exec(getEmail.value))){
 	 		var emailError = "Please enter a valid Email Address";
-		 	geteMail.style.border = "1px solid red";
+		 	getEmail.style.border = "1px solid red";
 		 	messageAry.push(emailError);
-		 }
+	}
 		 
-		 //if there were errors, display then on the screen
-		 if(messageAry.length >= 1){
-			 for(var i = 0, j = messageAry.length; i < j; i++){
+//if there were errors, display then on the screen
+		if(messageAry.length >= 1){
+			for(var i=0, j=messageAry.length; i < j; i++){
 				 var txt = document.createElement("li");
 				 txt.innerHTML = messageAry[i];
 				 errMsg.appendChild(txt);
-			 }
-			 e.preventDefault();
-			 return false;
-			  }else{
-			//if all is ok, save our data.  send the key value that came from the editData funcution
-			//remember the is was passed through the editSubmit
+			}
+			e.preventDefault();
+			return false;
+		}else{
+			
+//if all is ok, save our data.  send the key value that came from the editData funcution
+//remember the is was passed through the editSubmit
 			storeData (this.key);
-		 }
+		} 
+	
  	
 //Variable defaults
  	var jobGroups = ["--Choose A Group--", "GSR", "Housekeeper", "Maintenance"],
- 		shiftTime,
+ 		timeValue,
  		errMsg = $("errors");
- 	
- 	jobs();
-// 	shiftValue();
+ 	makeJobs();
+ 	timeValue();
  	sliderValue();
-// 	deleteItem();
+ 	deleteItem();
 
 //Set Link & Submit Click Events
- 	var showLink = $("showLink");
- 	showLink.addEventListener("click", getData);
+ 	var displayLink = $("showLink");
+ 	displayLink.addEventListener("click", getData);
  	var clear = $("clear");
  	clear.addEventListener("click", clearLocal);
  	var save = $("submit");
